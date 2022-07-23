2516b337a1c7189d20820e37dd67fef5873c6981
danfigueroa <danielmfigueroa@gmail.com>
2022-07-23 20:42:37 -0300

    test issue solved

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -5,7 +5,7 @@ describe('sample test', () => {
 
   it('displays the resources text', () => {
     cy.get('h1')
-    .contains('Welcome to my app!');
+    .contains('Mozão trabalhando aqui <3');
   })
   it('renders the Netlify logo image', () => {
     cy.get('img')
